2d310c60b726a34259f49791c6b571ff1d8ec2ef
vaspap1790 <vaspap1790@gmail.com>
2019-11-11 21:03:24 +0200

    please help

--- custom.js
+++ custom.js
@@ -1,25 +1,48 @@
 
-function addPhotos() {
+function addPhotos(e) {
+
+    $("<a href='#'><img id='photosPhoto' src='https://gitlab.gnome.org/uploads/-/system/project/avatar/295/gnome-photos.png'></a>").appendTo(e.target.parentElement);
 
 };
 
-function addNote() {
 
-    const title = $("#notesModal #noteTitle").val().trim();
-    const body = $("#notesModal #noteBody").val().trim();
-
-};
-
-function addPost() {
+function addPost(e) {
 
     const title = $("#postModal #postTitle").val().trim();
     const body = $("#postModal #postBody").val().trim();
 
 };
 
-function addBudget() {
+function addBudget(e) {
 
     const budget = $("#budgetModal #budget").val().trim();
+
+};
+
+function addNote(e) {
+    $("<a href='#'><img id='notePhoto' src='https://icon-library.net/images/icon-note/icon-note-0.jpg'></a>").appendTo(e.target.parentElement);
+};
+
+function clicked() {
+    console.log("clicked");
+
+};
+
+$("#addNote").click(clicked);
+
+function handleNote(e) {
+
+    const title = $("#notesModal #noteTitle").val().trim();
+    const body = $("#notesModal #noteBody").val().trim();
+    console.log(title, body);
+
+
+    $('#formNote').click(function () {
+        console.log(e.target.parentElement);
+
+        // $("<a href='#'><img id='notePhoto' src='https://icon-library.net/images/icon-note/icon-note-0.jpg'></a>").appendTo(e.target.parentElement);
+        e.target.parentElement.insertAdjacentHTML("afterend", "<a href='#'><img id='notePhoto' src='https://icon-library.net/images/icon-note/icon-note-0.jpg'></a>");
+    });
 
 };
 
@@ -28,34 +51,21 @@ $(document).ready(function () {
 
     $(".add").click(function (e) {
 
-        e.target.parentElement.append("Hello");
-
-
-    });
-
-
-
-    $(".add-choice").click(function () {
-
-        const clicked = this.name;
-        switch (clicked) {
+        const choice = this.id;
+        switch (choice) {
             case "photos":
-                console.log("photos");
+                addPhotos(e);
                 break;
             case "notes":
-                console.log("notes");
+                handleNote(e);
                 break;
             case "budget":
-                console.log("budget");
+                addBudget(e);
                 break;
+        }
 
-        };
     });
 
-    $("#addPhotos").click(addPhotos);
-    $("#addNote").click(addNote);
-    $("#addBudget").click(addBudget);
-    $("#addPost").click(addPost);
 
 });
 
